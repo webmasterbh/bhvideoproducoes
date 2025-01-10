d507f65a70b24785b81541a8152b854886e65131
wanderson <bhwsouza@gmail.com>
2025-01-10 16:05:24 -0300

    original cod

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -1,41 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
+
 import { render } from "@react-email/components";
+
 import { transporter, smtpEmail } from "@/utils/nodemailer";
+
 import { Email } from "@/components/email";
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
+  const { name, email, message } = body;
+
+  const emailHtml = render(
+    <Email name={name} email={email} message={message} />
+  );
+
+  const options = {
+    from: smtpEmail,
+    to: smtpEmail,
+    subject: "Novo email",
+    html: emailHtml,
+  };
+
   try {
-    const body = await req.json();
-    const { name, email, message } = body;
-
-    // Validação simples (substituir por uma validação mais robusta, se necessário)
-    if (!name || !email || !message) {
-      return NextResponse.json(
-        { error: "Todos os campos são obrigatórios." },
-        { status: 400 }
-      );
-    }
-
-    const emailHtml = render(
-      <Email name={name} email={email} message={message} />
-    );
-
-    const options = {
-      from: smtpEmail,
-      to: smtpEmail,
-      subject: "Novo email",
-      html: emailHtml,
-    };
-
-    // Envio do e-mail
-    await transporter.sendMail(options);
-
-    return NextResponse.json({ message: "E-mail enviado com sucesso." }, { status: 200 });
+    // Send email using the transporter
+    transporter.sendMail(options);
   } catch (error) {
-    console.error("Erro ao enviar e-mail:", error);
-    return NextResponse.json(
-      { error: "Falha ao enviar o e-mail." },
-      { status: 500 }
-    );
+    console.error("Falha ao enviar:", error);
   }
+  return new Response("OK");
 }
